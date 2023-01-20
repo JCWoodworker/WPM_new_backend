312b20a7793aebf0b348a6a98f6b2de8f4d6836d
JCWoodworker <jc@rilocalwoodworks.com>
2023-01-20 13:43:36 -0500

    setting up postgresql database .. this should definitely have been on a new branch, but oh well ...

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const cors = require('cors');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 bootstrap();
