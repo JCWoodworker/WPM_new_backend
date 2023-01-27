58e5a67f8d8c2487ff4891dcd04a23dd289abb0d
JCWoodworker <jc@rilocalwoodworks.com>
2023-01-27 12:24:36 -0500

    added tsconfig-paths/register import to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'tsconfig-paths/register'
+
 const cors = require('cors');
 
 async function bootstrap() {
