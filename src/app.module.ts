5a33a7e180ced0be2b3802fb4981be8768f36f1a
JCWoodworker <jc@rilocalwoodworks.com>
2023-02-18 12:46:34 -0500

    re-organized imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { UsersController } from './modules/users/controllers/users.controller';
 import { ProjectsModule } from './modules/projects/projects.module';
 import { ProjectsController } from './modules/projects/controllers/projects.controler'
-import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
