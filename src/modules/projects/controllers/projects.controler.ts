b6e326a646b5b1ad928ab273aaf00e907fb4663c
JCWoodworker <jc@rilocalwoodworks.com>
2023-02-18 11:06:05 -0500

    moved jwt guard to post

--- src/modules/projects/controllers/projects.controler.ts
+++ src/modules/projects/controllers/projects.controler.ts
@@ -8,17 +8,16 @@ import {
   Request,
 } from '@nestjs/common';
 
-import { LocalAuthGuard } from 'src/auth/local-auth.guard';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { ProjectInterface } from 'src/database/interfaces/project.interface';
 import { ProjectsService } from '../services/projects.service';
-@UseGuards(JwtAuthGuard)
 @Controller('projects')
 export class ProjectsController {
   constructor(
     private projectsService: ProjectsService,
-  ) {}
-
+    ) {}
+    
+  @UseGuards(JwtAuthGuard)
   @Post('newProject')
   async newProject(@Body() newProject: ProjectInterface) {
     return this.projectsService.createProject(newProject);
