d677a6dbe93a07c11c71353d4e0fe8c0ce8107ff
JCWoodworker <jc@rilocalwoodworks.com>
2023-05-21 02:26:34 -0400

    swapped back to * for cors

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ import 'dotenv/config'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-  }));
+  app.use(cors());
   await app.listen(port);
 }
 bootstrap();
