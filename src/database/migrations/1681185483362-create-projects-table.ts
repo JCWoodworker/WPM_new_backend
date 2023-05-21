7fda08d4e7bab79bb1727a33767278133589d5ed
JCWoodworker <jc@rilocalwoodworks.com>
2023-05-21 01:05:10 -0400

    updated migrations, interfaces, and entities

--- src/database/migrations/1681185483362-create-projects-table.ts
+++ src/database/migrations/1681185483362-create-projects-table.ts
@@ -3,17 +3,18 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 export class createProjectsTable1681185483362 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        CREATE TABLE projects (
-            projectId UUID PRIMARY KEY DEFAULT uuid_generate_v1(),
-            name VARCHAR NOT NULL,
-            customerId INTEGER DEFAULT NULL,
-            userId VARCHAR NOT NULL,
-            description VARCHAR NOT NULL,
-            quantity INTEGER NOT NULL,
-            laborHours INTEGER NOT NULL,
-            stage VARCHAR NOT NULL,
-            createdAt TIMESTAMP NOT NULL DEFAULT now()
-            )
+      CREATE TABLE "projects" (
+        "projectId" UUID PRIMARY KEY DEFAULT uuid_generate_v1(),
+        "name" VARCHAR NOT NULL,
+        "customerId" INTEGER DEFAULT NULL,
+        "userId" VARCHAR NOT NULL,
+        "description" VARCHAR NOT NULL,
+        "quantity" INTEGER NOT NULL,
+        "laborHours" INTEGER NOT NULL,
+        "stage" VARCHAR NOT NULL,
+        "createdAt" TIMESTAMP NOT NULL DEFAULT now()
+        "updatedAt" TIMESTAMP NOT NULL DEFAULT now()
+      )
     `);
   }
 
